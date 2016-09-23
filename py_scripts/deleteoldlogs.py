432a4606e5fa6347aa421cab17c1fab4cf0bc1d6
danifr <daferoes@gmail.com>
2016-09-23 09:49:55 +0200

    Fix encoding error when parsing XML

--- py_scripts/deleteoldlogs.py
+++ py_scripts/deleteoldlogs.py
@@ -9,6 +9,8 @@ EXPIRE_DAYS = 20
 TODAY = int(round(time.time() * 1000))
 EXPIRE_MILISECONDS = EXPIRE_DAYS * 24 * 60 * 60 * 1000
 
+PARSER = ET.XMLParser(encoding="utf-8")
+
 
 # API call to get the list of the existing projects on the server.
 def listProjects():
@@ -16,15 +18,15 @@ def listProjects():
     headers = {'Content-Type': 'application/json','X-RunDeck-Auth-Token': API_KEY }
     r = requests.get(url, headers=headers, verify=False)
     return r.text.encode('utf-8')
-    
+
 # Returns list of all the project names
 def getProjectNames(projectsinfo_xml):
     project_names = []
-    root = ET.fromstring(projectsinfo_xml)
-    for projects in root:	
+    root = ET.fromstring(projectsinfo_xml, parser=PARSER)
+    for projects in root:
         for name in projects.findall('project'):
             project_names.append(name.find('name').text)
-    return project_names   
+    return project_names
 
 # API call to get the list of the jobs that exist for a project.
 def listJobsForProject(project_mame):
@@ -37,13 +39,13 @@ def listJobsForProject(project_mame):
 # Returns list of all the jobids
 def getJobIDs(jobsinfo_xml):
     job_ids = []
-    root = ET.fromstring(jobsinfo_xml)	
+    root = ET.fromstring(jobsinfo_xml, parser=PARSER)
     for jobs in root:
         for job in jobs:
             job_ids.append(job.attrib['id'])
     return job_ids
 
-# API call to get the list of the executions for a Job.      
+# API call to get the list of the executions for a Job.
 def getExecutionsForAJob(job_id):
     url =  RUNDECKSERVER +':'+RUNDECKPORT+'/api/1/job/'+job_id+'/executions'
     headers = {'Content-Type': 'application/json','X-RunDeck-Auth-Token': API_KEY }
@@ -53,20 +55,20 @@ def getExecutionsForAJob(job_id):
 # Returns a dict {'execution_id01': 'execution_date01', 'execution_id02': 'execution_date02', ... }
 def getExecutionDate(executionsinfo_xml):
     execid_dates = {}
-    root = ET.fromstring(executionsinfo_xml)
-    for executions in root:     
+    root = ET.fromstring(executionsinfo_xml, parser=PARSER)
+    for executions in root:
         for execution in executions.findall('execution'):
             execution_id = execution.get('id')
             for date in execution.findall('date-ended'):
                 execution_date = date.get('unixtime')
     	    execid_dates[execution_id] = execution_date
     return execid_dates
-       
+
 #API call to delete an execution by ID
 def deleteExecution(execution_id):
     url =  RUNDECKSERVER +':'+RUNDECKPORT+'/api/12/execution/'+execution_id
     headers = {'Content-Type': 'application/json','X-RunDeck-Auth-Token': API_KEY }
-    r = requests.delete(url, headers=headers, verify=False)    
+    r = requests.delete(url, headers=headers, verify=False)
 
 def isOlderThanExpireDays(execution_date, today):
     if ((today - execution_date) > EXPIRE_MILISECONDS):
@@ -84,4 +86,4 @@ for project in projects:
     jobids = getJobIDs(listJobsForProject(project))
     for jobid in jobids:
         print '\tjobid:\t'+jobid
-        checkDeletion(getExecutionDate(getExecutionsForAJob(jobid))) 
+        checkDeletion(getExecutionDate(getExecutionsForAJob(jobid)))
